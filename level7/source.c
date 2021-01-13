044bcc0bfc1bc06266f6d2637518e752322d664a
Pablo Lamtenzan <pablolamtenzan@gmail.com>
2021-01-13 20:07:00 +0100

    level7

---
+++ level7/source.c
@@ -0,0 +1,40 @@
+
+# include <stdlib.h> // malloc
+# include <string.h> // strcpy
+# include <stdio.h> // fopen, fgest, printf
+# include <time.h> // time
+
+
+struct strct
+{
+	int		value;
+	char*	chars;
+};
+
+char data[0x44]; // 0x8049960
+
+void m()
+{
+	printf("%s - %d\n", data, time(0x0l));
+}
+
+int main(int ac, char** av)
+{
+	static const char modes[] = "r"; // 0x80486e9
+	static const char filename[] = "/home/user/level8/.pass"; // 0x80486eb
+	static const char display[] = "~~" // 0x8048703
+
+	struct strct*	s2; // {esp + 0x18}
+	struct strct*	s1; // {esp + 0x1c}
+	
+	s1 = malloc(sizeof(*s1));
+	*s1 = (struct strct){.value=0x1, .chars=malloc(sizeof(char) * 0x8ul)};
+
+	s2 = malloc(sizeof(*s2));
+	*s2 = (struct strct){.value=0x2, chars=malloc(sizeof(char) * 0x8ul)};
+
+	strcpy(s1->chars, av[1]);
+	strcpy(s2->chars, av[2]);
+	fgets(data, 0x44, fopen(filename, modes));
+	puts(display);
+}
