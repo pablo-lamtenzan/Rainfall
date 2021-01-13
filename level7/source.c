9e2f036fbdb94de59a29a3a875fb169674803d7d
Pablo Lamtenzan <pablolamtenzan@gmail.com>
2021-01-13 23:31:37 +0100

    bad order variables fixed

--- level7/source.c
+++ level7/source.c
@@ -7,25 +7,27 @@
 
 struct strct
 {
-	int		value;
 	char*	chars;
+	int		value;
 };
 
 char data[0x44]; // 0x8049960
 
 void m()
 {
-	printf("%s - %d\n", data, time(0x0l));
+	static const char format[] = "%s - %d\n"; // 0x8049960
+
+	printf(format, data, time(0x0l));
 }
 
 int main(int ac, char** av)
 {
 	static const char modes[] = "r"; // 0x80486e9
 	static const char filename[] = "/home/user/level8/.pass"; // 0x80486eb
-	static const char display[] = "~~" // 0x8048703
+	static const char display[] = "~~"; // 0x8048703
 
-	struct strct*	s2; // {esp + 0x18}
 	struct strct*	s1; // {esp + 0x1c}
+	struct strct*	s2; // {esp + 0x18}
 	
 	s1 = malloc(sizeof(*s1));
 	*s1 = (struct strct){.value=0x1, .chars=malloc(sizeof(char) * 0x8ul)};
